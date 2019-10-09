0ba64bdc02d5b8323717791da09bacd5c35c7d5a
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-09 12:57:22 -0700

    Added in axios URL with API key, and loaded img src, title, and explanation to the App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
+import PhotoHandler from "./Components/NasaPhoto";
 import "./App.css";
+import NasaPhoto from "./Components/NasaPhoto";
 
 function App() {
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        app! Have fun{" "}
+        <span role="img" aria-label="Space Ship">
+          🚀
+        </span>
+        !
       </p>
+
+      <NasaPhoto />
     </div>
   );
 }
---
+++ src/Components/NasaPhoto.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function NasaPhoto() {
+  const [photoData, setPhotoData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=70qZqLQhWqT4kF6rK4W2FDHLgzgveqdjc7oO8LW9"
+      )
+      .then(response => {
+        setPhotoData(response.data);
+      })
+      .catch(error => {
+        console.log("The data was not returned: ", error);
+      });
+  }, []);
+
+  return (
+    <div className="content-container">
+      <h3>{photoData.title}</h3>
+      <img src={photoData.hdurl} />
+      <p>{photoData.explanation}</p>
+    </div>
+  );
+}
