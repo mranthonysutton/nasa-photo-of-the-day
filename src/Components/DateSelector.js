199c715984393b00b8da8b021c732419cae93e82
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-09 13:38:38 -0700

    Added select date button and basic styles. Does not have functionality yet

---
+++ src/Components/DateSelector.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DateSelector = props => {
+  return <button className="dateSelector-button">Select New Date</button>;
+};
+
+export default DateSelector;
--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import DateSelector from "./DateSelector";
 
 export default function NasaPhoto() {
   const [photoData, setPhotoData] = useState([]);
+  const [dateSelector, setDateSelector] = useState("2019-10-06");
 
   useEffect(() => {
     axios
       .get(
-        "https://api.nasa.gov/planetary/apod?api_key=70qZqLQhWqT4kF6rK4W2FDHLgzgveqdjc7oO8LW9&date=2019-10-09"
+        `https://api.nasa.gov/planetary/apod?api_key=70qZqLQhWqT4kF6rK4W2FDHLgzgveqdjc7oO8LW9&date=${dateSelector}`
       )
       .then(response => {
         setPhotoData(response.data);
@@ -22,6 +24,7 @@ export default function NasaPhoto() {
       <h1>{photoData.title}</h1>
       <img className="api-img" src={photoData.hdurl} />
       <p>{photoData.explanation}</p>
+      <DateSelector />
     </div>
   );
 }
