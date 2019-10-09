5388fa8d9eb7f09c5cd4e7c17bf11f061b51b665
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-09 15:42:56 -0700

    State hook is set to the correct date format. Need to find out how to pass that into the setDateSelector

--- src/Components/DateSelector.js
+++ src/Components/DateSelector.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 
 const DateSelector = props => {
-  const [startDate, setStartDate] = useState(new Date());
+  const [dateSelector, setStartDate] = useState("2019-10-09");
 
   return (
     <div className="dateSelectorContainer">
       <h4>Select A Date</h4>
-      <input className="data-picker" type="date" max={startDate} />
-      <button>Submit</button>
+      <input
+        className="data-picker"
+        type="date"
+        value={dateSelector}
+        onChange={e => setStartDate(e.target.value)}
+      />
     </div>
   );
 };
