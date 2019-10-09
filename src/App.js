87ea4b0bedf81b75ea411c15d9995dc69a2d49bb
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-09 12:58:09 -0700

    Removed instruction text from App component

--- src/App.js
+++ src/App.js
@@ -6,15 +6,6 @@ import NasaPhoto from "./Components/NasaPhoto";
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun{" "}
-        <span role="img" aria-label="Space Ship">
-          🚀
-        </span>
-        !
-      </p>
-
       <NasaPhoto />
     </div>
   );
