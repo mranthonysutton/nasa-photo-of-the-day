468a7cec0ce74b7c0f4b19c02ea86a83181bfda5
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-10 12:17:25 -0700

    Imported styled-components and built function to obtain todays date and passed that into the dateSelector

--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import styled from "styled-components";
 import DateSelector from "./DateSelector";
 
 export default function NasaPhoto(props) {
+  // Function to grab today's date
+  const todayDate = () => {
+    let today = new Date();
+    const day = today.getDate();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+
+    today = `${year}-${month}-${day}`;
+    return today;
+  };
+
   const [photoData, setPhotoData] = useState({});
-  const [dateSelector, setDateSelector] = useState("2019-09-02");
+  const [dateSelector, setDateSelector] = useState(todayDate());
 
   useEffect(() => {
     axios
